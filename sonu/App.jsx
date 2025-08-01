7053b67e09d17d79ab09e8c40dcf250975b0064a
Siddhartho Biswas <siddharthobiswas2018@gmail.com>
2025-08-01 03:42:14 -1200

    Update App.jsx

--- sonu/App.jsx
+++ sonu/App.jsx
@@ -1,1 +1,18 @@
 
+import React  from 'react';
+import './App.css';
+import {Style,Text} from './components/style';
+import ImageComponent from './components/image';
+
+function App() {
+   
+  return (
+    <>
+      <Style />
+      <Text />
+      
+    </>
+  );
+}
+
+export default App;
