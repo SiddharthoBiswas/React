033db3f013fd412dd4725c119dcd0a6757b14e9b
Siddhartho Biswas <siddharthobiswas2018@gmail.com>
2025-08-01 21:15:13 +0530

    Add files via upload

---
+++ sonu/App.jsx
@@ -0,0 +1,18 @@
+import React  from 'react';
+import './App.css';
+import {Style,Text} from './components/style';
+import ImageComponent from './components/image';
+
+function App() {
+   
+
+  return (
+    <>
+      <Style />
+      <Text />
+      {/* <ImageComponent /> */}
+    </>
+  );
+}
+
+export default App;
