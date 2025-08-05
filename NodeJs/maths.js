394f40bfe44632e0a85b45997e184662f1c86ed9
Siddhartho Biswas <siddharthobiswas2018@gmail.com>
2025-08-05 12:25:18 +0530

    Create maths.js

---
+++ NodeJs/maths.js
@@ -0,0 +1,41 @@
+var a = 10
+var b = 20
+
+// substraction 
+var sub = a-b
+console.log (`Substraction of two number is ${sub}`)
+
+// multiplication
+var mul = a*b
+console.log(`Multiplication of two number is ${mul}`)
+
+//division
+var div = a/b
+console.log(`Division of two number is ${div}`)
+
+//area of a triangle
+
+var triangle = 0.5*a*b
+console.log(`area of a triangle is ${triangle}`)
+
+//area of rectangle 
+var rectangle = a*b
+console.log(`area of rectangle is ${rectangle}`)
+
+//area of a circle
+var circle = (4/3)*(22/7)*a**3
+console.log(`area of a circle is ${circle}`)
+
+//perimeter of a circle 
+var perimeter = 2*(22/7)*a
+console.log(`perimeter of a circle is ${perimeter}`)
+
+//area of a square 
+var square = a*a
+console.log(`area of a square is ${square}`)
+
+//area of a cube
+var cube = a**3
+console.log(`area of a cube is ${cube}`)
+
+
